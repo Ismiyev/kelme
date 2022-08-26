fbf37a7b988386595cc1dd80071b6c0dc9792b57
Ismiyev <105969918+Ismiyev@users.noreply.github.com>
2022-08-26 16:35:45 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()
 que = {}
 admins = {}
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5537646590:AAEwqO5MazvP_Kfmaf-bLdunbslxpO8s7KY")
-BOT_NAME = getenv("BOT_NAME", "CrocadilBot") 
-API_ID = int(getenv("API_ID" 13544181))
-API_HASH = getenv("API_HASH", "1cf5e591506286e82e89e98b2436ebb6")
-BOT_USERNAME = getenv("BOT_USERNAME", "Corcadildsidhlebot")
+BOT_TOKEN = getenv("5509121112:AAGLK7clrJGlJ7lsWpHsUWPne_HSpEh87xM")
+BOT_NAME = getenv("BOT_NAME", "𝙰𝚂𝙾🇦🇿 ⋖𝕊öℤü⋗ ⋖𝕋ᴀℙ⋗) 
+API_ID = int(getenv("API_ID" 15880833)
+API_HASH = getenv("API_HASH", "1524748e943315cd866fac7c4f81c5b2)
+BOT_USERNAME = getenv("BOT_USERNAME", "ASOsozutap_bot)
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://kenanbitcoin:PunnUo7gN6XpNidk@cluster0.gtmaj.mongodb.net/Cluster0?retryWrites=true&w=majority")
